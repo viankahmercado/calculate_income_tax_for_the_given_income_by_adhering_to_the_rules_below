c4a55707dba9f54e5c5518381abd7ce47f6fcd5c
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 22:05:57 +0800

    description

---
+++ exercise_12.py
@@ -0,0 +1,10 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 12:
+# Calculate income tax for the given income by adhering to the rules below
+
+# Expected Output: 
+# For example, suppose the taxable income is 45000, and the income tax payable is
+# 10000*0% + 10000*10%  + 25000*20% = $6000.
